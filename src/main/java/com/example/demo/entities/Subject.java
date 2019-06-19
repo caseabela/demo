4104b316eaf8140be1acfef6291aa0a9ab7439e7
Casey <caseabela@gmail.com>
2019-06-19 15:32:58 +0200

    Update Subject.java

--- src/main/java/com/example/demo/entities/Subject.java
+++ src/main/java/com/example/demo/entities/Subject.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -28,6 +29,7 @@ import org.hibernate.annotations.UpdateTimestamp;
  */
 
 @Entity
+@Table(name="subjects")
 public class Subject implements Serializable {
 	/**
 	 * 
